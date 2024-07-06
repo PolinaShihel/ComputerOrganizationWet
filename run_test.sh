76fcf7cea7015929842fecab2df25c6adfc04f64
palina <polinashihel@technion.campus.co.il>
2024-07-06 22:26:38 +0000

    removing prints

--- run_test.sh
+++ run_test.sh
@@ -8,15 +8,10 @@ as "$YOUR_ASM" "$TEST" -o merged.o
 if [ -f "merged.o" ]; then
 	ld merged.o -o merged.out
 	if [ -f "merged.out" ]; then
-		timeout 60s ./merged.out | tee tempOutput.txt
+		timeout 60s ./merged.out	
 		if [ $? -eq 0 ]; then
-			if cmp -s "$TEST.out" "tempOutput.txt"; then
-    			echo "${YOUR_ASM} tested with ${TEST_NAME}: PASS"
-			else
-    			echo "${YOUR_ASM} tested with ${TEST_NAME}: FAIL"
-			# echo "${YOUR_ASM} tested with ${TEST_NAME}: PASS"
+			echo "${YOUR_ASM} tested with ${TEST_NAME}: PASS"
 			STATUS=0
-			fi
 		else
 			echo "${YOUR_ASM} tested with ${TEST_NAME}: FAIL"
 			STATUS=1
@@ -31,5 +26,4 @@ else
 fi
 
 rm merged.*
-rm tempOutput.txt
 exit ${STATUS}
